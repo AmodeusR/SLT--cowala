5734963ecd8ba787b81c4186de5bcf17b5bcafc2
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-01-23 21:57:41 -0300

    Desafio Finalizado

--- desafio-frontend/src/Components/Form.jsx
+++ desafio-frontend/src/Components/Form.jsx
@@ -1,28 +1,71 @@
 import Input from "./Input";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Form = ({ title }) => {
   const [name, setName] = useState("");
   const [job, setJob] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [ipAddress, setIpAddress] = useState("");
-
+  
+  useEffect(() => {
+    getLocalStorage();
+  }, []);
+  
+  const getLocalStorage = () => {
+    setName(localStorage.name || "");
+    setJob(localStorage.job || "");
+    setPhoneNumber(localStorage.phoneNumber || "");
+    setIpAddress(localStorage.ipAddress || "");
+  }
+  
   const clearInputs = () => {
     setName("");
     setJob("");
     setPhoneNumber("");
     setIpAddress("");
+    localStorage.clear();
+    
+    alert("Seus dados foram apagados!")
   }
-
+  
   const handleFetch = () => {
     const ipApi = "https://ip-fast.com/api/ip/?format=json";
     fetch(ipApi)
-      .then(response => response.json())
-      .then(({ ip }) => setIpAddress(ip));
+    .then(response => response.json())
+    .then(({ ip }) => setIpAddress(ip));
   }
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(phoneNumber.length);
+    if (name.length === 0) {
+      alert("Insira um nome!")
+      return;
+
+    } else if (job.length === 0) {
+      alert("Insira uma profissão!")
+      return;
+
+    } else if (phoneNumber.length < 15) {
+      alert("Insira um número de telefone válido!");
+      return;
+    }
+    
+    const statesArray = [name, job, phoneNumber, ipAddress];
+    const keys = [
+      Object.keys({name})[0],
+      Object.keys({job})[0],
+      Object.keys({phoneNumber})[0],
+      Object.keys({ipAddress})[0]
+    ];
+    statesArray.forEach((state, i) => localStorage.setItem(keys[i], state));
+    alert("Seus dados foram salvos!")
+  }
+
+
+  
   return (
-    <form className="ip-form container" onSubmit={e => e.preventDefault()}>
+    <form className="ip-form container" onSubmit={(e) => handleSubmit(e)}>
       <h1 className="form-title">{title}</h1>
 
       <Input
